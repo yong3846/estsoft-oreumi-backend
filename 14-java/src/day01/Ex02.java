70785e05c9c962a43257ca2d7e10b64f06c6c7a8
GilYong Hyeon <gusrlfdyd12@gmail.com>
2025-11-28 16:14:51 +0900

    연습문제

---
+++ 14-java/src/day01/Ex02.java
@@ -0,0 +1,14 @@
+package day01;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 3;
+        System.out.printf("x: %d, y: %d%n", x, y);
+        System.out.printf("x + y: %d%n", x + y);
+        System.out.printf("x - y: %d%n", x - y);
+        System.out.printf("x * y: %d%n", x * y);
+        System.out.printf("x / y: %.2f%n", x / (double) y);
+        System.out.printf("x %% y: %d%n", x % y);
+    }
+}
